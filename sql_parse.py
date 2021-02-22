d990bde00585af4a3f2c92f2abfc45ea9b1b2efd
SungHwan-Bae <58796768+SungHwan-Bae@users.noreply.github.com>
2021-02-22 13:21:33 +0900

    Create sql_parse.py

---
+++ sql_parse.py
@@ -0,0 +1,234 @@
+import sys
+from pyparsing import *
+
+ParserElement.enablePackrat()
+
+LPAR, RPAR, COMMA = map(Suppress, "(),")
+DOT, STAR = map(Literal, ".*")
+select_stmt = Forward().setName("select statement")
+table_names = []
+columns_names = []
+
+# keywords
+keywords = {
+    k: CaselessKeyword(k)
+    for k in """\
+    UNION ALL AND INTERSECT EXCEPT COLLATE ASC DESC ON USING NATURAL INNER CROSS LEFT OUTER JOIN AS INDEXED NOT
+    SELECT DISTINCT FROM WHERE GROUP BY HAVING ORDER LIMIT OFFSET OR CAST ISNULL NOTNULL NULL IS BETWEEN ELSE END
+    CASE WHEN THEN EXISTS IN LIKE GLOB REGEXP MATCH ESCAPE CURRENT_TIME CURRENT_DATE CURRENT_TIMESTAMP TRUE FALSE
+    """.split()
+}
+vars().update(keywords)
+
+any_keyword = MatchFirst(keywords.values())
+
+quoted_identifier = QuotedString('"', escQuote='""')
+identifier = (~any_keyword + Word(alphas, alphanums + "_")).setParseAction(
+    pyparsing_common.downcaseTokens
+) | quoted_identifier
+collation_name = identifier.copy()
+column_name = identifier.copy()
+column_alias = identifier.copy()
+table_name = identifier.copy()
+table_alias = identifier.copy()
+index_name = identifier.copy()
+function_name = identifier.copy()
+parameter_name = identifier.copy()
+database_name = identifier.copy()
+
+comment = "--" + restOfLine
+
+# expression
+expr = Forward().setName("expression")
+
+numeric_literal = pyparsing_common.number
+string_literal = QuotedString("'", escQuote="''")
+blob_literal = Regex(r"[xX]'[0-9A-Fa-f]+'")
+literal_value = (
+    numeric_literal
+    | string_literal
+    | blob_literal
+    | TRUE
+    | FALSE
+    | NULL
+    | CURRENT_TIME
+    | CURRENT_DATE
+    | CURRENT_TIMESTAMP
+)
+bind_parameter = Word("?", nums) | Combine(oneOf(": @ $") + parameter_name)
+type_name = oneOf("TEXT REAL INTEGER BLOB NULL")
+
+expr_term = (
+    CAST + LPAR + expr + AS + type_name + RPAR
+    | EXISTS + LPAR + select_stmt + RPAR
+    | function_name.setName("function_name")
+    + LPAR
+    + Optional(STAR | delimitedList(expr))
+    + RPAR
+    | literal_value
+    | bind_parameter
+    | Group(
+        identifier("col_db") + DOT + identifier("col_tab") + DOT + identifier("col")
+    )
+    | Group(identifier("col_tab") + DOT + identifier("col"))
+    | Group(identifier("col"))
+)
+
+NOT_NULL = Group(NOT + NULL)
+NOT_BETWEEN = Group(NOT + BETWEEN)
+NOT_IN = Group(NOT + IN)
+NOT_LIKE = Group(NOT + LIKE)
+NOT_MATCH = Group(NOT + MATCH)
+NOT_GLOB = Group(NOT + GLOB)
+NOT_REGEXP = Group(NOT + REGEXP)
+
+UNARY, BINARY, TERNARY = 1, 2, 3
+expr << infixNotation(
+    expr_term,
+    [
+        (oneOf("- + ~") | NOT, UNARY, opAssoc.RIGHT),
+        (ISNULL | NOTNULL | NOT_NULL, UNARY, opAssoc.LEFT),
+        ("||", BINARY, opAssoc.LEFT),
+        (oneOf("* / %"), BINARY, opAssoc.LEFT),
+        (oneOf("+ -"), BINARY, opAssoc.LEFT),
+        (oneOf("<< >> & |"), BINARY, opAssoc.LEFT),
+        (oneOf("< <= > >="), BINARY, opAssoc.LEFT),
+        (
+            oneOf("= == != <>")
+            | IS
+            | IN
+            | LIKE
+            | GLOB
+            | MATCH
+            | REGEXP
+            | NOT_IN
+            | NOT_LIKE
+            | NOT_GLOB
+            | NOT_MATCH
+            | NOT_REGEXP,
+            BINARY,
+            opAssoc.LEFT,
+        ),
+        ((BETWEEN | NOT_BETWEEN, AND), TERNARY, opAssoc.LEFT),
+        (
+            (IN | NOT_IN) + LPAR + Group(select_stmt | delimitedList(expr)) + RPAR,
+            UNARY,
+            opAssoc.LEFT,
+        ),
+        (AND, BINARY, opAssoc.LEFT),
+        (OR, BINARY, opAssoc.LEFT),
+    ],
+)
+
+compound_operator = UNION + Optional(ALL) | INTERSECT | EXCEPT
+
+ordering_term = Group(
+    expr("order_key")
+    + Optional(COLLATE + collation_name("collate"))
+    + Optional(ASC | DESC)("direction")
+)
+
+join_constraint = Group(
+    Optional(ON + expr | USING + LPAR + Group(delimitedList(column_name)) + RPAR)
+)
+
+join_op = COMMA | Group(
+    Optional(NATURAL) + Optional(INNER | CROSS | LEFT + OUTER | LEFT | OUTER) + JOIN
+)
+
+join_source = Forward()
+single_source = (
+    Group(database_name("database") + DOT + table_name("table*") | table_name("table*"))
+    + Optional(Optional(AS) + table_alias("table_alias*"))
+    + Optional(INDEXED + BY + index_name("name") | NOT + INDEXED)("index")
+    | (LPAR + select_stmt + RPAR + Optional(Optional(AS) + table_alias))
+    | (LPAR + join_source + RPAR)
+)
+
+join_source <<= (
+    Group(single_source + OneOrMore(join_op + single_source + join_constraint))
+    | single_source
+)
+
+# result_column = "*" | table_name + "." + "*" | Group(expr + Optional(Optional(AS) + column_alias))
+result_column = Group(
+    STAR("col")
+    | table_name("col_table") + DOT + STAR("col")
+    | expr("col") + Optional(Optional(AS) + column_alias("alias"))
+)
+
+select_core = (
+    SELECT
+    + Optional(DISTINCT | ALL)
+    + Group(delimitedList(result_column))("columns")
+    + Optional(FROM + join_source("from*"))
+    + Optional(WHERE + expr("where_expr"))
+    + Optional(
+        GROUP
+        + BY
+        + Group(delimitedList(ordering_term))("group_by_terms")
+        + Optional(HAVING + expr("having_expr"))
+    )
+)
+
+select_stmt << (
+    select_core
+    + ZeroOrMore(compound_operator + select_core)
+    + Optional(ORDER + BY + Group(delimitedList(ordering_term))("order_by_terms"))
+    + Optional(
+        LIMIT
+        + (Group(expr + OFFSET + expr) | Group(expr + COMMA + expr) | expr)("limit")
+    )
+)
+
+select_stmt.ignore(comment)
+
+def main():
+    tests = """
+       SELECT E.ENTNAME                              AS "엔터티명"
+     , AA.ATTRNAME                            AS "부모속성명"
+     , A.ATTRNAME                             AS "자식속성명"
+        FROM DA_RELATIONSHIP R
+        , DA_DERIVEDCONT D
+        , DA_ATTRIBUTE A
+        , DA_ATTRIBUTE AA
+        , DA_ENTITY E
+        WHERE 1=1
+        AND R.REFAREAID       = D.REFAREAID
+        AND R.RELID           = D.RELID
+        AND R.EXPIREDDATE     = D.EXPIREDDATE
+        AND D.REFAREAID       = A.REFAREAID
+        AND D.REFDERIVEDID    = A.ATTRID
+        AND D.EXPIREDDATE     = A.EXPIREDDATE
+        AND R.EXPIREDDATE     = '99999999999999'
+        AND D.REFAREAID       = AA.REFAREAID
+        AND D.REFDERIVINGID   = AA.ATTRID
+        AND D.EXPIREDDATE     = AA.EXPIREDDATE
+        AND A.REFAREAID  = E.REFAREAID
+        AND A.REFENTID   = E.ENTID
+        AND AA.ATTRNAME = '프로젝트코드'
+    """
+    print("_____________")
+    result = select_stmt.parseString(tests)
+    visit_structure(result)
+    print(table_names)    
+    print(columns_names)
+
+def visit_structure(struct):
+       
+    #    print("struct : ", struct)
+       
+       if 'table' in struct:
+        #    print("tbl22 : ", struct)
+           table_names.extend(t[0] for t in struct.table)
+       if 'col' in struct:
+        #    print("col22 : ", struct)
+            for c in struct.col:
+                print(c)
+        #    columns_names.extend(c[0] for c in struct.col)
+       for substruct in struct:
+           if isinstance(substruct, ParseResults):
+               visit_structure(substruct)
+
+if __name__ == "__main__":
+    main()
